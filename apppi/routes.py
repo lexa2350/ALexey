88997deb9f2db7788dcc847fa37e54368efd2896
User <sv.alexey00@mail.ru>
2022-10-27 13:20:52 +0300

    СДЕЛАЛ СТРАНИЦУ О СЕБЕ

--- apppi/routes.py
+++ apppi/routes.py
@@ -2,13 +2,13 @@ from apppi import app
 from flask import render_template
 from random import choice
 
-menu = [{"name": 'Главная', "url": 'index'}, {"name": 'О программе', "url": 'about'}, {"name": 'Помощь', "url": 'help'}]
+menu = [{"name": 'Главная', "url": 'index'}, {"name": 'О программе', "url": 'about'}, {"name": 'Помощь', "url": 'help'}, {"name": 'ЛЕХА2350', "url": 'LEXA'}]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    best_pi = {'username': 'Алексей'}
+    best_pi = {'username': 'Алексей  '}
 
     return render_template('index.html', title='2022 Forever', user=best_pi, menu=menu)
 
@@ -22,3 +22,7 @@ def help():
 @app.route('/about')
 def about():
     return render_template('about.html', menu=menu)
+
+@app.route('/LEXA')
+def LEXA():
+    return render_template('LEXA2350.html', menu=menu)
